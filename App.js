7d0530a7c8f737e101ec8919c7a728a4c7ac3b58
appaaaa <appaaaa@naver.com>
2017-10-30 17:52:03 +0900

    albumdetail 부터 이어서

--- App.js
+++ App.js
@@ -2,12 +2,16 @@
 
 // Import a library to help create a component
 import React from 'react';
-import { View, Text } from 'react-native';
-import Header from './src/components/header';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
  
 // Create a component
 const App = () => (
-    <Header headerText={'Albums! '} />
+  <View>
+    <Header headerText={'Albums!'} />
+    <AlbumList />
+  </View>
 );
     
 
